460e96bdfc7c55c18bd6e8566e0871db4e168cef
Alvin Chandy <chipmunk353@gmail.com>
2015-06-15 21:51:18 -0400

    Completed Exercise 09 Objects

--- 09-objects/00-basics/challenge.js
+++ 09-objects/00-basics/challenge.js
@@ -1,7 +1,43 @@
-module.exports.createCourse = undefined;
+module.exports.createCourse = function(title, duration, students) {
+  var createCourse = {
+    title: title,
+    duration: duration,
+    students: students
+  }
+  return createCourse
+};
 
-module.exports.addProperty = undefined;
+module.exports.addProperty = function(object, newProp, newValue) {
+  if ( !object.hasOwnProperty(newProp) ) {
+    object[newProp] = newValue;
+  }
+  return object;
+};
 
-module.exports.formLetter = undefined;
 
-module.exports.canIGet = undefined;
+module.exports.formLetter = function(data) {
+  return 'Hello ' + data.recipient + ',\n\n' + data.msg + '\n\nSincerely,\n' + data.sender;
+};
+
+
+module.exports.canIGet = function(item,money) {
+  var priceTable = {
+    'MacBook Air': 999,
+    'MacBook Pro': 1299,
+    'Mac Pro': 2499,
+    'Apple Sticker': 1
+  };
+
+  // Assume item === "Jason's Giant Thing"
+  // Now, Assume item === "Mac Pro"
+  // if ( priceTable.hasOwnProperty(item) === false ) { ... }
+  return (priceTable.hasOwnProperty(item)) && (money >= priceTable[item]);
+};
+
+
+
+/*if (object[newProp] = newValue) {
+  return object;
+}
+};
+*/
